d376d236ab5c2ecfd54ffe40fff6da507422910e
Daniel Washington <danielvertigo@hotmail.com>
2021-06-14 09:12:43 -0300

    Files

--- IstheStringEmpty/ViewController.swift
+++ IstheStringEmpty/ViewController.swift
@@ -5,6 +5,24 @@
 //  Created by Daniel Washington Ignacio on 14/06/21.
 //
 
+/*
+ Create a function that returns true if a string is empty and false otherwise.
+
+ Examples
+
+ isEmpty("") ➞ true
+
+ isEmpty(" ") ➞ false
+
+ isEmpty("a") ➞ false
+ Notes
+
+ A string containing only whitespaces " " does not count as empty.
+ Don't forget to return the result.
+ If you get stuck on a challenge, find help in the Resources tab.
+ If you're really stuck, unlock solutions in the Solutions tab.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +30,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.isEmpty(""))
+        print(self.isEmpty(" "))
+        print(self.isEmpty("a"))
     }
 
+    func isEmpty(_ str: String) -> Bool {
+        return (str.count == 0)
+    }
 
 }
 
